eaa8d59d25af32bdde25fb442b475045f23bd619
Hayden Longster <COMP5276352@gccomp.local>
2025-02-03 16:19:36 +0000

    first commit

---
+++ UserInfo/name.cs
@@ -0,0 +1,83 @@
+using System;
+
+namespace UserInfo
+{
+    class Program
+    {
+        static bool appup()
+        {
+            Console.WriteLine("Do you want to continue? (Y/N)");
+            string online = Console.ReadLine();
+            if (online == "Y")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static string GetName()
+        {
+            static string firstncheck()
+            {
+                Console.WriteLine("Please enter your first name (Max of 20 characters):");
+                string firstn = Console.ReadLine();
+                firstn = firstn.ToLower();
+                return firstn;
+            }
+            static string lastncheck()
+            {
+                Console.WriteLine("Please enter your last name (Max of 20 characters):");
+                string lastn = Console.ReadLine();
+                lastn = lastn.ToUpper();
+                return lastn;
+            }
+            string firstn = firstncheck();
+            string lastn = lastncheck();
+            if (firstn.Length > 20 || lastn.Length > 20)
+            {
+                Console.WriteLine("Name too long, please try again.");
+                return "retry";
+            }
+            else
+            {
+                return string.Concat(firstn, " ", lastn);
+            }
+        }
+        static int GetAge()
+        {
+            static int agecheck()
+            {
+                Console.WriteLine("Please enter your age (must be between 18 and 80):");
+                int age = Convert.ToInt32(Console.ReadLine());
+                return age;
+            }
+            int age = agecheck();
+            if (age < 18 || age > 80)
+            {
+                Console.WriteLine("Age out of range, please try again.");
+                return 0;
+            }
+            else
+            {
+                return age;
+            }
+        }
+        static void Main(string[] args)
+        {
+            string Uname = GetName();
+            while (Uname == "retry")
+            {
+                Uname = GetName();
+            }
+            Console.WriteLine($"Full name: {Uname}");
+            int age = GetAge();
+            while (age == 0)
+            {
+                age = GetAge();
+            }
+            Console.WriteLine($"Welcome {Uname}, you are {age} years old.");
+        }
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        // gets a choice from the user for what type of equation they want
+        static string GetString()
+        {
+            Console.WriteLine("Enter enter a choice of a type below (must include caps):");
+            Console.WriteLine("Add, Minus,Divide, Multiply, Sqrt");
+            string choice = Console.ReadLine();
+            return choice;
+        }
+        // gets two numbers from the user for use in later equations
+        static int[] GetNums()
+        {
+            Console.WriteLine("Enter two numbers for the sum. Numbers must be whole.");
+            int inp1 = Convert.ToInt32(Console.ReadLine());
+            int inp2 = Convert.ToInt32(Console.ReadLine());
+            return new int[] { inp1, inp2 };
+        }
+        // ask user if they want to continue if not return false and kills app
+        static bool appup()
+        {
+            Console.WriteLine("Do you want to continue? (Y/N)");
+            string online = Console.ReadLine();
+            if (online == "Y")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            do
+            {
+                string choice = GetString();
+                if (choice == "Add")
+                {
+                    int[] nums = GetNums();
+                    int sum = nums[0] + nums[1];
+                    Console.WriteLine("The sum is " + sum);
+                }
+                else if (choice == "Minus")
+                {
+                    int[] nums = GetNums();
+                    int sum = nums[0] - nums[1];
+                    Console.WriteLine("The difference is " + sum);
+                }
+                else if (choice == "Divide")
+                {
+                    int[] nums = GetNums();
+                    int sum = nums[0] / nums[1];
+                    Console.WriteLine("The quotient is " + sum);
+                }
+                else if (choice == "Multiply")
+                {
+                    int[] nums = GetNums();
+                    int sum = nums[0] * nums[1];
+                    Console.WriteLine("The product is " + sum);
+                }
+                else if (choice == "Sqrt")
+                {
+                    Console.WriteLine("Enter the number for the square root. Number must be whole.");
+                    int SqrtInp = Convert.ToInt32(Console.ReadLine());
+                    int sum = (int)Math.Sqrt(SqrtInp);
+                    Console.WriteLine("The square root is " + sum);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input.");
+                }
+            }
+            while (appup() == true);
+        }
+    }
+}

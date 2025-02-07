686638c955b28253c7142c4c1f2bafbac3b5125b
Gloshayden <Gloshayden@gmail.com>
2025-02-07 12:53:22 +0000

    making a note app

---
+++ noteapp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+
+namespace noteapp
+{
+    class Program
+    {
+        static Boolean NoteFolder()
+        {
+            if (!Directory.Exists("notes"))
+            {
+                System.IO.Directory.CreateDirectory("notes");
+                Console.WriteLine("Folder Created");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Folder Already Exists");
+                return true;
+            }
+        }
+        static string[] GetNotes()
+        {
+            if (!Directory.EnumerateFileSystemEntries("notes").Any())
+            {
+                Console.WriteLine("No notes found");
+                return null;
+            }
+            else
+            {
+                string[] notes = Directory.GetFiles("notes");
+                return notes;
+            }
+        }
+        static void Main(string[] args)
+        {
+            NoteFolder();
+            string[] notes = GetNotes();
+            for (int i = 0; i < notes.Length; i++)
+            {
+                Console.WriteLine(notes[i]);
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

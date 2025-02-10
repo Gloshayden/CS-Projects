80d80e484962e3dcee94beff4bede85b7bdf3950
Gloshayden <Gloshayden@gmail.com>
2025-02-10 14:47:05 +0000

    finished the note app

--- noteapp/Program.cs
+++ noteapp/Program.cs
@@ -1,15 +1,12 @@
-﻿using System.Formats.Asn1;
-using System.IO;
-
-namespace noteapp
+﻿namespace noteapp
 {
     class Program
     {
         static bool appup()
         {
-            Console.WriteLine("Do you want to continue? (Y/N)");
-            string online = Console.ReadLine();
-            if (online == "Y")
+            Console.WriteLine("Do you want to continue? (y/n)");
+            string online = Console.ReadLine()!;
+            if (online == "y")
             {
                 return true;
             }
@@ -25,17 +22,13 @@ namespace noteapp
                 System.IO.Directory.CreateDirectory("notes");
                 Console.WriteLine("Folder Created");
             }
-            else
-            {
-                Console.WriteLine("Folder Already Exists");
-            }
         }
         static string[] GetNotes()
         {
             if (!Directory.EnumerateFileSystemEntries("notes").Any())
             {
                 Console.WriteLine("No notes found");
-                return null;
+                return null!;
             }
             else
             {
@@ -46,15 +39,15 @@ namespace noteapp
         static void AddNote()
         {
             Console.WriteLine("Enter the name of the note");
-            string note = Console.ReadLine();
+            string note = Console.ReadLine()!;
             string path = $"notes/{note}.txt";
             using (File.Create(path)) { }
             Console.WriteLine("Do you want to add a description to the note? (y/n)");
-            string answer = Console.ReadLine();
+            string answer = Console.ReadLine()!;
             if (answer == "y")
             {
                 Console.WriteLine("Enter the description");
-                string description = Console.ReadLine();
+                string description = Console.ReadLine()!;
                 using (StreamWriter writer = File.AppendText(path))
                 {
                     writer.WriteLine(description);
@@ -65,11 +58,11 @@ namespace noteapp
         {
             Console.WriteLine("Enter the one of the following commands");
             Console.WriteLine("Edit Description, Delete Note, Edit title, Back");
-            string command = Console.ReadLine();
+            string command = Console.ReadLine()!;
             if (command == "Edit Description")
             {
                 Console.WriteLine("Enter the new description");
-                string description = Console.ReadLine();
+                string description = Console.ReadLine()!;
                 File.WriteAllText(path, description);
                 return "new description added";
             }
@@ -81,7 +74,7 @@ namespace noteapp
             else if (command == "Edit title")
             {
                 Console.WriteLine("Enter the new title");
-                string title = Console.ReadLine();
+                string title = Console.ReadLine()!;
                 string newpath = $"notes/{title}.txt";
                 File.Move(path, newpath);
                 path = newpath;
@@ -99,31 +92,36 @@ namespace noteapp
             {
                 NoteFolder();
                 string[] notes = GetNotes();
+                while (notes == null) 
+                {
+                   Console.WriteLine("Do you want to add a note? (y/n)");
+                    string nonoteanswer = Console.ReadLine()!;
+                    if (nonoteanswer == "y") { AddNote(); } 
+                    notes = GetNotes();
+                }
                 for (int i = 0; i < notes.Length; i++)
                 {
                     Console.WriteLine(notes[i].Remove(0,6));
                 }
                 Console.WriteLine("Do you want to add a note? (y/n)");
-                string answer = Console.ReadLine();
-                if (answer == "y")
-                {
-                    AddNote();
-                }
+                string answer = Console.ReadLine()!;
+                if (answer == "y") { AddNote(); }
                 Console.WriteLine("Do you want to read a note? (y/n)");
-                answer = Console.ReadLine();
+                answer = Console.ReadLine()!;
                 if (answer == "y")
                 {
                     Console.WriteLine("Enter the name of the note you want to read");
-                    string note = Console.ReadLine();
+                    string note = Console.ReadLine()!;
                     string path = $"notes/{note}.txt";
                     string[] lines = File.ReadAllLines(path);
                     for (int i = 0; i < lines.Length; i++)
                     {
                         Console.WriteLine(lines[i]);
+                        Console.WriteLine();
                     }
-                    while (EditNote(path) != "back")
+                    while (EditNote(path).ToLower() != "back")
                     {
-                        Console.WriteLine(EditNote(path));
+                        path = EditNote(path);
                     }
                 }
             } while (appup() == true);
